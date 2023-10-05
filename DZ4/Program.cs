39e492b8d853dfbfc084b7634f2febfd176c9f29
Гульназ Калимуллина <gulnaz.kalimullina.2005@mail.ru>
2023-10-05 22:35:33 +0300

    Добавьте файлы проекта.

---
+++ DZ4/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        public static int Numbers(int a, int b)
+        {
+            if (a > b) { return a; }
+            else { return b; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упр 5.1\nНаписать метод, возвращающий наибольшее из двух чисел.");
+            Console.WriteLine("Введите 2 числa через <Enter>");
+            int A, B, Final;
+            bool i = int.TryParse(Console.ReadLine(), out A);
+            bool i0 = int.TryParse(Console.ReadLine(), out B);
+
+            Final = Numbers(A, B);
+            Console.WriteLine(Final);
+
+
+
+
+            Console.WriteLine("Упр 5.2\nНаписать метод, который меняет местами значения двух передаваемых параметров.");
+            Console.WriteLine("Значение первого числа = 3, значение второго числа = 4");
+            Console.WriteLine("Нажмите <Enter> чтобы поменять значения местами");
+            int C = 3;
+            int D = 4;
+            Value(ref C, ref D);
+
+
+
+            Console.WriteLine("\nУпр 5.3\nНаписать метод вычисления факториала числа");
+            Console.WriteLine("Введите число");
+            int num;
+            bool j = int.TryParse(Console.ReadLine(), out num);
+            if (num >= 0)
+            {
+                bool flag = Factorial(ref num);
+                Console.WriteLine(Convert.ToString(flag) + $"\nФакториал введённого числа равен {num}");
+            }
+            else { Console.WriteLine("Факториал отрицательного числа не существует"); }
+
+
+
+
+            Console.WriteLine("Упр 5.4\nНаписать рекурсивный метод вычисления факториала числа");
+            Console.WriteLine("Введите число");
+            int number = int.Parse(Console.ReadLine());
+            int fact = Factorial(number);
+            Console.WriteLine($"Факториал введённого числа равен {fact}");
+
+
+            Console.WriteLine("DZ_5.1\nЗаданние с НОДом для 2 чисел");
+            Console.WriteLine("Введите два числа через <Enter>");
+            int a1 = int.Parse(Console.ReadLine());
+            int b1 = int.Parse(Console.ReadLine());
+            NOD(ref a1, ref b1);
+
+            Console.WriteLine("Дз 5.1\nЗаданние с НОДом для 3 чисел");
+            Console.WriteLine("Введите три числа через <Enter>");
+            int a2 = int.Parse(Console.ReadLine());
+            int b2 = int.Parse(Console.ReadLine());
+            int c2 = int.Parse(Console.ReadLine());
+            NOD(ref a2, ref b2, ref c2);
+
+
+
+            Console.WriteLine("Дз 5.2\nНаписать рекурсивный метод, вычисляющий значение n-го числа ряда Фибоначчи.");
+            int n = int.Parse(Console.ReadLine());
+            int fib = Fibonachi(n);
+            Console.WriteLine(fib);
+        }
+
+        public static void NOD(ref int G, ref int D)
+        {
+            while (G != 0 && D != 0)
+            {
+                if (G > D)
+                {
+                    G = G % D;
+                }
+                else
+                {
+                    D = D % G;
+                }
+            }
+            Console.WriteLine(D + G);
+        }
+        public static void NOD(ref int G, ref int D, ref int Q)
+        {
+            while (G != 0 && D != 0 && Q != 0)
+            {
+                if (G > D)
+                {
+                    G = G % D;
+                }
+                else
+                {
+                    D = D % G;
+                }
+                int R = G + D;
+                if (R > Q)
+                {
+                    R = R % Q;
+                }
+                else
+                {
+                    Q = Q % R;
+                }
+                int nod = R + Q;
+                Console.WriteLine(nod);
+            }
+        }
+
+        public static void Value(ref int a, ref int b)
+        {
+            int temple = a;
+            a = b;
+            b = temple;
+            Console.WriteLine($"Первое значение = {a}, второе значение = {temple}");
+        }
+        static bool Factorial(ref int num)
+        {
+            int result = num;
+            num = 1;
+            for (int i = 1; i <= result; i++)
+                try
+                {
+                    checked
+                    {
+                        num *= i;
+                    }
+                }
+                catch { return false; }
+            return true;
+
+        }
+
+        static int Factorial(int a)
+        {
+            if (a == 0) return 1;
+            return a * Factorial(a - 1);
+        }
+
+        static int Fibonachi(int n, int p1 = 0, int p2 = 1)
+        {
+            if (n <= 1) return p1;
+            int p;
+            for (int j = 2; j < n; j++)
+            {
+                p = p1;
+                p1 = p2;
+                p2 = p2 + p;
+            }
+            return p2;
+        }
+    }
+}
---
+++ Usersss/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Usersss
+{
+    internal class Program
+    {
+        enum Grouchiness
+        {
+            Ворчун_бабай,
+            Осуждающий,
+            Матур_бабай,
+            Мозай_бабай
+        }
+        struct Дед
+
+        {
+            public string name;
+            public Grouchiness level;
+            public string[] Phrases;
+            public int bruises;
+            public void GrandmaHits(params string[] SwearWords)
+            { 
+               foreach (string word in Phrases)
+                {
+                    if (Array.IndexOf(SwearWords, word.ToLower()) != -1)
+                    {
+                        bruises++;
+                    }
+                }
+            }
+        }
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упр 3\nПользователь вводит числа, в зависимости от значения которых что-нибудь происходит");
+            Console.WriteLine("Введите число или закройте программу");
+            try
+            {
+                string func1 = Console.ReadLine();
+                if (func1.ToLower() == "exit" || func1.ToLower() == "закрыть") { }
+                else
+                {
+                    int func = Convert.ToInt32(func1);
+                    if (func >= 0 && func <= 9)
+                    {
+                        switch (func)
+                        {
+                            case 0: Console.WriteLine("###\n# #\n# #\n# #\n###"); break;
+                            case 1: Console.WriteLine(" #\n## \n #\n #\n###"); break;
+                            case 2: Console.WriteLine(" #\n# #\n  #\n #\n###"); break;
+                            case 3: Console.WriteLine("###\n  #\n #\n  #\n###"); break;
+                            case 4: Console.WriteLine("# #\n# #\n###\n  #\n  #"); break;
+                            case 5: Console.WriteLine("###\n#\n###\n  #\n###"); break;
+                            case 6: Console.WriteLine("###\n#\n###\n# #\n###"); break;
+                            case 7: Console.WriteLine("###\n  #\n ##\n  #\n  #"); break;
+                            case 8: Console.WriteLine("###\n# #\n###\n# #\n###"); break;
+                            case 9: Console.WriteLine("###\n# #\n###\n  #\n###"); break;
+                            default:
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Ошибка");
+                                Thread.Sleep(3000);
+                                Console.BackgroundColor = ConsoleColor.Black; break;
+                        }
+                    }
+
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы совершили не предусмотренное действие");
+            }
+
+            Console.WriteLine("Упр 4\nНаписать метод, который на вход принимает деда. Вернуть количество фингалов.");
+            string[] SwearWords = { "мат1", "мат2", "мат3", "мат4", "мат5", "мат6" };
+
+
+            Дед Макар = new Дед();
+            Макар.name = "Макар";
+            Grouchiness Mgrouchinesslvl = Grouchiness.Ворчун_бабай;
+            Макар.level = Mgrouchinesslvl;
+            string[] MakarPhrases = { "мат1", "мат2", "мат3", "мат4", "мат5", "Хулиганы", "Фашисты", "Дэбилы" };
+            Макар.Phrases = MakarPhrases;
+            Макар.bruises = 0;
+            Макар.GrandmaHits(SwearWords);
+
+
+            Дед Илья = new Дед();
+            Илья.name = "Илья";
+            Grouchiness Igrouchinesslvl = Grouchiness.Матур_бабай;
+            Илья.level = Igrouchinesslvl;
+            string[] IlyaPhrases = { "мат1","Хулиганы","Потерянное поколение","Неучи"};
+            Илья.Phrases = IlyaPhrases;
+            Илья.bruises = 0;
+            Илья.GrandmaHits(SwearWords);
+
+
+            Дед Василий = new Дед();
+            Василий.name = "Василий";
+            Grouchiness Vgrouchinesslvl = Grouchiness.Ворчун_бабай;
+            Василий.level = Vgrouchinesslvl;
+            string[] VasilyPhrases = { "мат1","мат2","мат3","Дэбилы", "Неучи", "Гады" };
+            Василий.Phrases = VasilyPhrases;
+            Василий.bruises = 0;
+            Василий.GrandmaHits(SwearWords);
+
+
+            Дед Данил = new Дед();
+            Данил.name = "Данил";
+            Grouchiness Dgrouchinesslvl = Grouchiness.Осуждающий;
+            Данил.level = Dgrouchinesslvl;
+            string[] DanilPhrases = { "мат1", "мат2", "Неучи", "Гады", "Фашисты" };
+            Данил.Phrases = DanilPhrases;
+            Данил.bruises = 0;
+            Данил.GrandmaHits(SwearWords);
+
+
+            Дед Пётр = new Дед();
+            Пётр.name = "Пётр";
+            Grouchiness Pgrouchinesslvl = Grouchiness.Мозай_бабай;
+            Пётр.level = Pgrouchinesslvl;
+            string[] PetrPhrases = { "Мат1", "Потерянное поколение" };
+            Пётр.Phrases = PetrPhrases;
+            Пётр.bruises = 0;
+            Пётр.GrandmaHits(SwearWords);
+
+            Console.WriteLine($"Дед {Макар.name} получил {Макар.bruises} фингалов\nДед {Илья.name} - {Илья.bruises}\nДед {Василий.name} - {Василий.bruises}\nДед {Данил.name} - {Данил.bruises}\nДед {Пётр.name} - {Пётр.bruises}");
+        }
+    }
+}
